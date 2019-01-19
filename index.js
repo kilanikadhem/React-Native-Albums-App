6d28e178c7d0ed89dddd631ca84e5a7475c64114
kilanikadhem <kilani_kadhem@outlook.fr>
2019-01-19 15:34:22 +0100

    Make header component Reusable

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 //create Component
 const App = () => (
-       <Header />
+       <Header headerText={'Albums'} />
    );
 
 
